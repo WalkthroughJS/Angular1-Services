6cdc5b91829c3aa2f11679dae0e6393371ecfc20
John Armbruster <jaboyak@gmail.com>
2016-11-04 10:39:31 -0700

    updated lesson

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var app = angular.module('myFirstNgApp', []);
 
-app.controller('myFirstController', function($scope) {
-
+app.controller('myFirstController', function($scope, $http) {
+  $scope.makeAPIcall = function(character) {
+    $http.get('https://swapi.co/api/people/?search=' + character)
+     .then(function(api_response) {
+       console.log(api_response);
+       $scope.results = api_response.data.results;
+     });
+  }
 });
